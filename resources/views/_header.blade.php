96e24ed0dec16376611e31086d2c1d4494ad88d6
LeoDorbes <leodorbes@gmail.com>
2016-11-07 10:40:42 +0100

    Added bootstrap

---
+++ resources/views/_header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="fr">
+    <meta charset="UTF-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link href="css/main.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+    <title>Projet Evenement</title>
+</head>
+<body>
+
+
+</body>
+
+
+</html>
