fe5f598be83e1dd266ca310fd14fae188fe88f25
MSI\nicolas <n.tiesenhausen@gmail.com>
2016-11-08 12:05:23 +0100

    debut css page formulaire

--- resources/views/front/registration.blade.php
+++ resources/views/front/registration.blade.php
@@ -2,14 +2,30 @@
 
 @section('content')
 
-    <h2> S'inscrire à l'event </h2>
-    <?= Former::open()->class('form_horizontal')->method('GET') ?>
-    <?= Former::text('first_name')->label(false)->placeholder('Prénom')->required() ?>
-    <?= Former::text('last_name')->label(false)->placeholder('Nom')->required() ?>
-    <button type="submit" class="btn btn-primary">
-        <i class="fa fa-btn fa-user"></i> s'inscrire
-    </button>
-    <?= Former::close() ?>
+    <div class="row" id="subscription-form">
+        <div class="col-sm-offset-3 col-sm-6">
+            <h2> S'inscrire à l'event </h2>
+            <?= Former::open()
+                    ->rules(array(
+                           'postal' => ''
+
+                    ))
+
+
+                    ->class('form_horizontal')->method('GET') ?>
+            <?= Former::text('first_name')->label(false)->placeholder('Prénom')->required() ?>
+            <?= Former::text('last_name')->label(false)->placeholder('Nom')->required() ?>
+            <?= Former::email('email')->label(false)->placeholder('Email')->required() ?>
+            <?= Former::text('adresse')->label(false)->placeholder('Adresse')->required() ?>
+            <?= Former::text('Ville')->label(false)->placeholder('Ville')->required() ?>
+            <?= Former::text('postal')->label(false)->placeholder('Code Postal')->required() ?>
+
+            <button type="submit" class="btn btn-primary">
+                <i class="fa fa-btn fa-user"></i> s'inscrire
+            </button>
+            <?= Former::close() ?>
+        </div>
+    </div>
 
 @endsection
 
