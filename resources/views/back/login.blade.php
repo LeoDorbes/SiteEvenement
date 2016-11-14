e2224cc4678d97826f2fddc0ead75f7e09cbdb32
MSI\nicolas <n.tiesenhausen@gmail.com>
2016-11-14 10:56:52 +0100

    on continue ... ( rien de magique )

--- resources/views/back/login.blade.php
+++ resources/views/back/login.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 
-    <div class="row" id="subscription-form">
+    <div class="row form subscription-form">
 
         <div class="col-md-4 col-md-offset-4">
 
             <h2>Connexion à l'admin :</h2>
 
             <?= Former::open(route('login_process'))->class('form_horizontal')->method('POST') ?>
-            <?= Former::text('login')->label(false)->placeholder('Login')->required() ?>
-            <?= Former::password('password')->label(false)->placeholder('Mot de Passe')->required() ?>
-            <button type="submit" class="btn btn-primary">
+            <?= Former::text('login')->label(false)->placeholder('Login')->required()->class('form-control form-li') ?>
+            <?= Former::password('password')->label(false)->placeholder('Mot de Passe')->required()->class('form-control form-li') ?>
+            <button type="submit" class="btn btn-primary" id="bouton-login">
                 <i class="fa fa-btn fa-user"></i> Connexion
             </button>
             <?= Former::close() ?>
