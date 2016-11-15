e48c3182459284b6ef55072fb4f51b0b7f4b50b5
LeoDorbes <leodorbes@gmail.com>
2016-11-15 10:13:55 +0100

    Added all the routes for the backend

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,27 @@
 |
 */
 
-    /*
-     * Front Routes :
-     */
-    Route::get('/' , ['as' => 'home' , 'uses' => 'FrontController@index']);
-    Route::get('inscription' , ['as' => 'registration' , 'uses' => 'FrontController@registration']);
-    Route::post('inscription/process' , ['as' => 'registration_process' , 'uses' => 'FrontController@registrationProcess']);
+/*
+ * Front Routes :
+ */
+Route::get('/', ['as' => 'home', 'uses' => 'FrontController@index']);
+Route::get('inscription', ['as' => 'registration', 'uses' => 'FrontController@registration']);
+Route::post('inscription/process', ['as' => 'registration_process', 'uses' => 'FrontController@registrationProcess']);
 
-    /*
-    * Back Routes :
-    */
+/*
+* Back Routes :
+*/
 
-    Route::post('login' , ['as' => 'login_process' , 'uses' => 'BackController@loginProcess']);
-    Route::get('premier-utilisateur' , ['as' => 'first_account' , 'uses' => 'BackController@firstUser']);
-    Route::get('admin' , ['as' => 'login' , 'uses' => 'BackController@login']);
-    Route::get('admin/index' , ['as' => 'admin_home' , 'uses' => 'BackController@index']);
+Route::post('login', ['as' => 'login_process', 'uses' => 'BackController@loginProcess']);
+Route::get('premier-utilisateur', ['as' => 'first_account', 'uses' => 'BackController@firstUser']);
+Route::get('admin', ['as' => 'login', 'uses' => 'BackController@login']);
+Route::get('admin/index', ['as' => 'admin_home', 'uses' => 'BackController@index']);
+Route::get('admin/index/subscriptions', ['as' => 'admin_subscriptions', 'uses' => 'BackController@subscriptions']);
+Route::get('admin/edit/subscription/{id}', ['as' => 'admin_edit_subscription', 'uses' => 'BackController@editSubscription']);
+Route::post('admin/edit/subscription/{id}/process', ['as' => 'admin_edit_subscription_process', 'uses' => 'BackController@editSubscriptionProcess']);
+Route::get('admin/add/subscription/{id}', ['as' => 'admin_add_subscription', 'uses' => 'BackController@addSubscription']);
+Route::post('admin/add/subscription/{id}/process', ['as' => 'admin_add_subscription_process', 'uses' => 'BackController@addSubscription']);
+Route::get('admin/delete/subscription/{id}', ['as' => 'admin_delete_subscription_process', 'uses' => 'BackController@deleteSubscriptionProcess']);
 
 
 
