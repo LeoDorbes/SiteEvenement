ab619171a90637e55d6cc23ac4a949199c6d3429
LeoDorbes <leodorbes@gmail.com>
2016-11-22 12:16:31 +0100

    added page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('front/_template')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row form subscription-form">
+            <div class="col-sm-offset-3 col-sm-6">
+
+
+                <h2> Erreur 404 : Page introuvable :-( </h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/front/registration.blade.php
+++ resources/views/front/registration.blade.php
@@ -3,30 +3,30 @@
 @section('content')
 
     <div class="container-fluid">
-    <div class="row form subscription-form">
-        <div class="col-sm-offset-3 col-sm-6">
-            <h2> S'inscrire à l'event </h2>
-            <?= Former::open_vertical(route('registration_process'))->method('POST')
-            ->rules(array(
-                   'postal' => ''
+        <div class="row form subscription-form">
+            <div class="col-sm-offset-3 col-sm-6">
+                <h2> S'inscrire à l'event </h2>
+                <?= Former::open_vertical(route('registration_process'))->method('POST')
+                        ->rules(array(
+                                'postal' => ''
 
-            ))
-            ->class('form_horizontal') ?>
-            <?= Former::text('first_name')->label(false)->placeholder('Prénom')->required()->class('form-control form-li') ?>
-            <?= Former::text('last_name')->label(false)->placeholder('Nom')->required()->class('form-control form-li') ?>
-            <?= Former::email('email')->label(false)->placeholder('Email')->required()->class('form-control form-li') ?>
-            <?= Former::text('address')->label(false)->placeholder('Adresse')->required()->class('form-control form-li') ?>
-            <?= Former::text('city')->label(false)->placeholder('Ville')->required()->class('form-control form-li') ?>
-            <?= Former::text('postal_code')->label(false)->placeholder('Code Postal')->required()->class('form-control form-li') ?>
-            <?= Former::text('position')->label(false)->placeholder('Votre fonction')->class('form-control form-li') ?>
-            <?= Former::textarea('comment')->label(false)->placeholder('Commentaire')->required()->class('form-control form-li') ?>
+                        ))
+                        ->class('form_horizontal') ?>
+                <?= Former::text('first_name')->label(false)->placeholder('Prénom')->required()->class('form-control form-li') ?>
+                <?= Former::text('last_name')->label(false)->placeholder('Nom')->required()->class('form-control form-li') ?>
+                <?= Former::email('email')->label(false)->placeholder('Email')->required()->class('form-control form-li') ?>
+                <?= Former::text('address')->label(false)->placeholder('Adresse')->required()->class('form-control form-li') ?>
+                <?= Former::text('city')->label(false)->placeholder('Ville')->required()->class('form-control form-li') ?>
+                <?= Former::text('postal_code')->label(false)->placeholder('Code Postal')->required()->class('form-control form-li') ?>
+                <?= Former::text('position')->label(false)->placeholder('Votre fonction')->class('form-control form-li') ?>
+                <?= Former::textarea('comment')->label(false)->placeholder('Commentaire')->required()->class('form-control form-li') ?>
 
-            <button type="submit" class="btn btn-primary">
-                <i class="fa fa-btn fa-user"></i> s'inscrire
-            </button>
-            <?= Former::close() ?>
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-btn fa-user"></i> s'inscrire
+                </button>
+                <?= Former::close() ?>
+            </div>
         </div>
-    </div>
     </div>
 
 @endsection
